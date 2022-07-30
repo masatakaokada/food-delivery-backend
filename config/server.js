75a59e6efbc73aaceaaaf69fe375160a5a3a81f4
Masataka Okada <masataka.okada1117@gmail.com>
2022-07-30 12:54:18 +0900

    Strapiのアプリケーション作成

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1c54cb603dbd9c9288120bdbecb71942'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d56b385-bc8e-4c07-9045-7bde576e89a2'
+};
